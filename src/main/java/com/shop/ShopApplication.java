dac32425c7216e8c841f915d5ce761c7ea8e6684
오주리 <ojl98@naver.com>
2024-03-29 11:49:09 +0900

    application.yaml에 mysql과 jpa설정

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,8 +2,12 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/* @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) 
+	DB를 설정하지 않았을 때 JPA 설정 묶어두기
+*/
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ShopApplication {
 
 	public static void main(String[] args) {
