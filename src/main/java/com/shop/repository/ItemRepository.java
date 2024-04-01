45a2be3d911f903e7cbb94173d5b6110d88c8d23
오주리 <ojl98@naver.com>
2024-04-01 11:40:01 +0900

    ItemRepository 인터페이스 추가하고 insert 테스트

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,16 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+//JpaRepository 첫번째는 엔터티타입, 두번째는 기본키타입
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+}
---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+//@TestPropertySource(locations="classpath:application-test.properties")
+public class ItemRepositoryTest {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemTest(); //위의 메소드 실행
+        //interface 안에 선언한 @Query 사용 메소드 실행
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+}
